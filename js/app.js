2f669f76885acd5e0fd6be31b860cb3d8246534c
katherinelef <katherine.lef@hotmail.com>
2018-01-19 10:34:09 -0500

    añadiendo practica firebase

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyAJwphqoWvjY42O9GvPHuzVymzFKEGIhpk",
+   authDomain: "aprendiendo-farebase.firebaseapp.com",
+   databaseURL: "https://aprendiendo-farebase.firebaseio.com",
+   projectId: "aprendiendo-farebase",
+   storageBucket: "",
+   messagingSenderId: "716631715075"
+ };
+ firebase.initializeApp(config);
